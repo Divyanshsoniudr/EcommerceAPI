bdf221fe4c1be200993f83d9028a95fb290f8a1d
divyansh soni <dxdivyansh.soni@gmail.com>
2023-04-04 21:42:12 +0530

    Backend working started

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app= express();
 const mongoose = require("mongoose")
 const dotenv = require("dotenv");
-const userRoute = require("./routes/user")
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
@@ -15,7 +16,8 @@ mongoose.connect(
     console.log(err);
 });
 
-
+app.use(express.json());
+app.use("/api/auth", authRoute);
 app.use("/api/user", userRoute);
 
 app.listen(process.env.PORT || 5000, ()=>{
