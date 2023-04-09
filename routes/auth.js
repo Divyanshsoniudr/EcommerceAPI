05061fa3a0f4926389ba22c1a4c362ff28246801
divyansh soni <dxdivyansh.soni@gmail.com>
2023-04-09 16:12:57 +0530

    New update

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,12 @@
 const router= require("express").Router();
 const User = require("../models/User")
-
+const CryptoJS = require("crypto-js")
 //REGISTER
 router.post("/register", async (req,res)=> {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: CCryptoJS.AES.encryp(req.body.password, process.env.PASS_SEC).toString,
 
     });
     try{
@@ -16,6 +16,16 @@ router.post("/register", async (req,res)=> {
     res.status(500).json(err);
     }
 });
+
+//LOGIN
+
+router.post("/login",async (req,res)=>{
+
+    
+})
+
+
+
 
 
 module.exports = router;
