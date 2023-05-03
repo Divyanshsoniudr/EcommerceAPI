c584650cfb2654fd44b46390fcb08bd9e8a45ede
divyanshsoniudr <dxdivyansh.soni@gmail.com>
2023-05-03 10:14:13 +0530

    All changess 29/04/23

--- models/Order.js
+++ models/Order.js
@@ -1,31 +1,24 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-// Define the order schema
 const OrderSchema = new mongoose.Schema(
-    {
-        // ID of the user who made the order
-        userId:{type:String, required: true}, 
-        // An array of products that were ordered, each with its own product ID and quantity
-        products:[
-            {
-                productId:{
-                    type: String,
-                },
-                quantity: {
-                    type: Number,
-                    default: 1,
-                },
-            },
-        ],
-        // Total amount of the order
-        amount: {type:Number, required:true},
-        // Address where the order will be shipped
-        address:{type:Object , required: true},
-        // Status of the order (e.g. pending, shipped, delivered, etc.)
-        status: {type: String, default: "pending"}
-    },
-    {timestamps:true}
+  {
+    userId: { type: String, required: true },
+    products: [
+      {
+        productId: {
+          type: String,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
+    amount: { type: Number, required: true },
+    address: { type: Object, required: true },
+    status: { type: String, default: "pending" },
+  },
+  { timestamps: true }
 );
 
-// Export the order model
-module.exports = mongoose.model("Order",OrderSchema);
+module.exports = mongoose.model("Order", OrderSchema);
